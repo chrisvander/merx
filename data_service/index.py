ee3fe233f27f3c36d6f107eee4645ec4a8590c39
Michael van der Loo <vanderloo.michael@yahoo.com>
2020-09-19 13:37:39 -0400

    bond request working

--- data_service/index.py
+++ data_service/index.py
@@ -5,19 +5,28 @@ URL Format: <url>/?mode=stock&stock=<stock>&beg=<start time>&<end time>&int=<int
 http://localhost:5000/stock/?stock=APPL&start=2013-04-23&end=2013-04-24
 '''
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import quandl
 
 app = Flask(__name__)
 quandl.ApiConfig.api_key = 'uyxvXKZ3zKytArHDVdwR'
 
-@app.route('/', methods=['GET'])
-def query():
-  input = request.args
+@app.route('/stock-history/', methods=['GET'])
+def get_history():
+  return request_stock(request.args)
 
-  output = request_stock(input)
+@app.route('/stock-tickers/', methods=['GET'])
+def get_tickers():
+  return jsonify(['HD','DIS','MSFT','BA','MMM','PFE','NKE','JNJ','MCD','INTC','XOM','GS','JPM','AXP','V','IBM','UNH','PG','GE','KO','CSCO','CVX','CAT','MRK','WMT','VZ','UTX','TRV','AAPL'])
 
-  return output
+@app.route('/bonds/', methods=['GET'])
+def get_bonds():
+  df = quandl.get_table('CHORD7/BD', date='2013-05-01', qopts={'columns':['date', 'ask_price']})
+  print(df.index)
+  # for col in df.columns:
+  #   if col != 'date' or col != 'ask_price':
+  #     df.drop(col, axis=1)
+  return df.to_json()
 
 def request_stock(input):
   if 'stock' in input:
@@ -25,18 +34,22 @@ def request_stock(input):
   else:
     return 'error: must provide stock ticker'
 
+  df = quandl.get("EOD/" + stock)
+
   if 'start' in input:
     start = input['start']
   else:
-    start = None
+    start = str(df.index[0])[:10]
 
   if 'end' in input:
     end = input['end']
   else:
-    end = None
+    end = str(df.index[-1])[:10]
 
-  return quandl.get(["EOD/" + stock, "Close"], start_date="2017-12-26", end_date="2017-12-28").to_json()
-''', start_date=start, end_date=end'''
+  json = quandl.get("EOD/" + stock, start_date=start, end_date=end)["Close"].to_json()
+
+
+  return json
 
 if __name__ == "__main__":
   app.run(debug=False, port=5000)
